6a05072a9fd24bc392a437891116650fb3c225a6
vithu302001 <pavalakumaran.21@cse.mrt.ac.lk>
2023-10-25 18:53:27 +0530

    commit 1

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -1,11 +1,29 @@
 #ifndef USERPROG_PROCESS_H
 #define USERPROG_PROCESS_H
 
+
+#include "threads/synch.h"
 #include "threads/thread.h"
+
+#include <user/syscall.h>
+
 
 tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
+
+struct  description_file{
+    struct list_elem element;
+    int file_id;
+    struct file *ptr_file;
+
+  
+}
+
+
+//creating structure for PCB(process control block)
+
+
 
 #endif /* userprog/process.h */
